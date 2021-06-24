2820bae337e75224f668848752a49f31e3e5dba2
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2021-06-25 02:42:01 +0300

    Update odyssey.php

--- pages/odyssey.php
+++ pages/odyssey.php
@@ -33,7 +33,7 @@
                   <div class="item-title">
                     Odyssey
                   </div><a href=
-                  "itms-services://?action=download-manifest&url=https://rebo-store.com/install/odyssey.php"
+                  "itms-services://?action=download-manifest&url=https://rebo-store.com/install/odyssey.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
