5addcf868dec50aea8359f8dabad55db8b52f36d
SeeYouI0S <seeyouios2021@gmail.com>
2021-06-25 01:06:29 +0300

    initial

---
+++ x/js/routes.js
@@ -0,0 +1,39 @@
+routes = [
+  {
+    path: '/',
+    url: '/store/',
+  },
+  {
+    path: '/:id/',
+    url: '/section/{{id}}',
+  },
+  {
+    path: '/odyssey',
+    url: './pages/odyssey.php',
+  },
+  {
+    path: '/unc0ver',
+    url: './pages/unc0ver.html',
+  },
+  {
+    path: '/videostar',
+    url: './pages/videostar.php',
+  },
+  {
+    path: '/pubg',
+    url: './pages/pubg.php',
+  },
+  {
+    path: '/8ball',
+    url: './pages/8ball.php',
+  },
+  {
+    path: '/appcake',
+    url: './pages/appcake.php',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.php',
+  },
+];
